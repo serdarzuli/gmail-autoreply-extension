7881a74b63369c4552021034763ca6fd193b5418
ZuliSerdar <serdar.zuli@gmail.com>
2025-04-09 16:07:19 +0200

    moved prompt to .env

--- backend/app/openai_service.py
+++ backend/app/openai_service.py
@@ -1,23 +1,21 @@
 from openai import OpenAI
 from app.config import settings
+import os
+from dotenv import load_dotenv
 
+#.env load file
+load_dotenv()
 
 async def generate_reply(mail_body: str) -> str:
     client = OpenAI(api_key=settings.OPENROUTER_API_KEY, base_url=settings.BASE_URL_ROUTER_API)
+    prompt = os.getenv("prompt")
 
     response = client.chat.completions.create(
             model="openai/gpt-3.5-turbo-0613",   
             messages=[
             {
                 "role": "user",
-                "content": 
-                f"""
-                Below is an email received from a customer.
-                Provide a professional and polite response that directly addresses the message. 
-                Answer any questions clearly and completely. 
-                Avoid unnecessary repetition and vague expressions. 
-                End the response with a friendly closing that leaves the door open for further communication.
-
+                "content": f"""{prompt} 
                 Incoming email: 
                 \"\"\"
                 {mail_body}
